42bc5b3c00011f6ae90f6a4455ff1b7e2eba93d0
Anwar Hossain <cseanwar@gmail.com>
2026-05-21 17:43:39 +0200

    Modified Fetured card

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -9,33 +9,20 @@ const Featured = async () => {
 
   return (
     <div className="my-10 px-4 lg:px-0 lg:max-w-7xl lg:mx-auto">
-
       {/* Header row */}
-      <div className="flex flex-col sm:flex-row sm:items-center justify-between gap-4">
-        <div>
-          <h1 className="text-4xl md:text-5xl lg:text-6xl text-[#0C0B0B] font-normal mb-2">
-            Featured Facilities
-          </h1>
-          <p className="text-[#6C696D] text-base md:text-lg">
-            Handpicked travel experiences for the adventure seekers
-          </p>
-        </div>
-
-        <Link href={"/destinations"} className="shrink-0">
-          <Button
-            variant="outline"
-            className="rounded-2xl px-6 py-6 bg-linear-to-r from-[#22C55E] to-[#16A34A] w-full sm:w-auto"
-          >
-            ALL FACILITIES
-            <FaArrowRightLong />
-          </Button>
-        </Link>
+      <div className="flex flex-col gap-10 justify-center items-center">
+        <h1 className="text-4xl md:text-5xl lg:text-6xl text-[#0C0B0B] font-normal mb-2">
+          Featured Facilities
+        </h1>
+        <p className="text-[#6C696D] text-base md:text-lg">
+          Handpicked travel experiences for the adventure seekers
+        </p>
       </div>
 
       {/* Grid */}
-      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5 mt-10">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-5 mt-10">
         {facilities.map((facility) => (
-          <FacilityCard key={facility._id} destination={facility} />
+          <FacilityCard key={facility._id} facility={facility} />
         ))}
       </div>
     </div>
